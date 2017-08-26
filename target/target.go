3d416c855786c6ecdd461db1e4f782911289b6e1
Samuel Ribeiro <ribeiro@google.com>
2017-08-26 12:02:12 +0200

    Change Get Value string.

--- target/target.go
+++ target/target.go
@@ -7,7 +7,7 @@ func ReflectGetRequest(request *gnmi.GetRequest) *gnmi.GetResponse {
 	response := gnmi.GetResponse{Notification: []*gnmi.Notification{}}
 	notification := gnmi.Notification{Update: []*gnmi.Update{}}
 	for _, path := range request.Path {
-		typedValue := gnmi.TypedValue{Value: &gnmi.TypedValue_StringVal{StringVal: "TESTDATA"}}
+		typedValue := gnmi.TypedValue{Value: &gnmi.TypedValue_StringVal{StringVal: "TEST STRING"}}
 		update := gnmi.Update{Path: path, Val: &typedValue}
 		notification.Update = append(notification.Update, &update)
 	}
